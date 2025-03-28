663b2d12861b6b484e49aa73ed2b255cba88ea05
João Costa <joaocostagr@outlook.pt>
2025-03-28 22:12:56 +0000

    solving

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // O Vercel espera um diretório "dist"
+    outDir: "frontend/dist", // O Vercel espera um diretório "dist"
   },
   server: {
     port: 3000
