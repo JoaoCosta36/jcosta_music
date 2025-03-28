a298baeba4e79337f41be5ace718034c64ad53ea
João Costa <joaocostagr@outlook.pt>
2025-03-28 21:32:00 +0000

    solving..

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'frontend/dist', // Este é o diretório onde os arquivos de build serão gerados
+    outDir: 'dist', // Este é o diretório onde os arquivos de build serão gerados
   },
 });
