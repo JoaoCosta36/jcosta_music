736fee8b945032e3e54f4230a6f92293c68d966b
João Costa <joaocostagr@outlook.pt>
2025-03-28 00:18:25 +0000

    correcting

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist',  // Certifique-se que o build será para a pasta 'dist'
+    outDir: 'frontend/dist',  // Certifique-se que o build será para a pasta 'dist'
   },
 });
